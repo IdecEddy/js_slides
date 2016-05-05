cc350b66e7ba40fb17649dc52c966c8cc5600422
eddy <eddy@debbox>
2016-05-05 18:54:07 -0400

    alot was added here.     - you can now loop through images with images_array just add the img       string to it.
        - added some comments on how the whole script works.
    
        ~next I need to move the script and css into there own files and set
        up a demo file for the whole project to look cleaner.

--- slide_show.php
+++ slide_show.php
@@ -1,4 +1,3 @@
-
 <html>
     <head>
         <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
@@ -56,29 +55,38 @@
         </style>
 
         <script>
-            
-
+            //images_array is where we hold all the images we we want to cycle through. 
+            var images_array = ["test1.jpg","test2.jpg","test3.jpg"];         
             var top_slide = true;
             var animating = false;
-
-      
-            function move_slide1()
-            {
+            // -1 so because JS counts from 1 when getting length.
+            var images_array_length = images_array.length - 1;
+            var images_array_count = 0;
+            // the two functions that handel moving the slides position after/before a transition.
+            function move_slide1(){
                 $('#slide2').insertBefore('#slide1');
             }
             
-            function move_slide2()
-            {
+            function move_slide2(){
                 $('#slide1').insertBefore('#slide2');
             }
 
             $(document).ready(function(){
                 $('#next_slide').on('click',function(e){
+                    // if we are in the middel of a transition dont start another one.
                     if(animating === true) { return;} 
                     animating = true;
-                        
+
+                    // this is where we check where we are in the array and see if we need to loop around or keep going.
+                    if(images_array_count < images_array_length){
+                        images_array_count++;
+                    } else if (images_array_count >= images_array_length){
+                        images_array_count = 0;
+                    }
+                    // if the top slide is 1 then we move slide1 to the right end of the container and prep it for a transition
                     if(top_slide === true){
                         
+                        $("#slide2").css('background-image', 'url(' + images_array[images_array_count] + ')');
                         $('#slide1').animate({width:"0%"},1000, function(){
                             animating = false;
                             move_slide1();           
@@ -87,8 +95,9 @@
                             animating = false;
                         });
                         top_slide = false;
-                    }
-                    else{
+                    } else {
+                        //else we do the reverse.
+                        $("#slide1").css('background-image', 'url(' + images_array[images_array_count] + ')');
                         $('#slide2').animate({width:"0%"}, 1000, function(){
                             animating = false;
                             move_slide2();           
@@ -99,12 +108,21 @@
                         top_slide = true;
                     }
                 });
+            
                 $("#last_slide").on("click", function(e){ 
+                    //basicly the same as above but in reverse for the back button.
                     if(animating === true){ return;}
                     animating = true;
- 
+                    
+                    if(images_array_count > 0){
+                        images_array_count--;
+                    } else if (images_array_count <= 0){
+                        images_array_count = images_array_length;
+                    }
+                    console.log("images_array_count: " + images_array_count + " images_array_length: " + images_array_length );
                     if(top_slide === true){
                         move_slide1();
+                        $("#slide2").css('background-image', 'url(' + images_array[images_array_count] + ')');
                         $("#slide2").animate({width:"100%"},1000, function(){
                             animating = false;
                         });
@@ -115,6 +133,7 @@
                     }
                     else{
                         move_slide2();
+                        $("#slide1").css('background-image', 'url(' + images_array[images_array_count] + ')');
                         $("#slide2").animate({width:"0%"},1000, function(){
                             animating = false;                                  
                         });
@@ -126,6 +145,7 @@
                 });
             });
         </script>
+    
     </head>
     <body>
         <div id="main">
